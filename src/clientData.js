13baff256fefe56f9379869c549415f4fc1128db
Abhishek Ghodke <abhighodke3@gmail.com>
2024-08-31 13:36:23 +0530

    client updated

--- src/clientData.js
+++ src/clientData.js
@@ -1,38 +1,31 @@
 // src/clientData.js
 const clientData = {
-    headerInitials: "LC", // Header Initials
-    heroTagline: "Linda Chrisp Realty", // Hero Tagline
-    agentName: "Linda Chrisp",
-    aboutMeShort: "I specialize in finding perfect homes for buyers and sellers, including investors and veterans. My expertise and dedication to exceptional service guide you to the best decisions. Let's find your ideal home together.", // AboutMe Short Description
-    aboutMeDetailed: `Linda Chrisp is a highly experienced REALTOR® and Associate Broker at Maximum One Greater Atlanta Realtors, residing in Marietta, with a strong background in real estate spanning over 15 years. Her dedication, attention to detail, and personalized approach make her a reliable professional in the field.
+    headerInitials: "LJ",
+    heroTagline: "Laura Jelks Realty",
+    agentName: "Laura Jelks",
+    aboutMeShort: "I specialize in finding perfect homes for buyers and sellers, including investors and veterans. My expertise and dedication to exceptional service guide you to the best decisions. Let's find your ideal home together.",
+    aboutMeDetailed: `It is my passion to assist clients with reaching their goal of owning a home or selling their current home to find a new one. It is important to me that my clients find the ideal home to match their needs and their budget. Also, I have helped many investors find the perfect property to complete their investment portfolio. 
 
-Linda is a native of South Carolina and holds a BS Degree in Office Management and Administration from South Carolina State University. Additionally, she has a certified paralegal certificate from the National Center for Paralegal Training in Atlanta, Georgia. Her diverse educational background and legal expertise contribute to her comprehensive understanding of real estate transactions.
-Linda primarily serves the counties of Cobb, North Fulton, South Fulton, Paulding, Douglas, and Cherokee, showcasing her commitment to these local communities. Her specialization extends to working with veterans and their families, a passion fueled by her marriage to a military veteran of over 28 years.
+With my knowledge and experience I provide my clients accurate and complete detailed information so they can make the best decisions. Also, with being certified to help our Veterans and their families in finding a home and utilizing their full VA eligibility. Allow me to assist you with your real estate needs.
 
-Outside of her professional commitments, Linda is a devoted mother and a member of Delta Sigma Theta Sorority, Inc. and WENU (Women Entrepreneurship Network United). These affiliations reflect her engagement with both her community and networks that support women entrepreneurs.
-
-In summary, Linda Chrisp is a dedicated and knowledgeable real estate professional with a diverse background, offering top-notch services to sellers, buyers, landlords, and veterans in the Greater Atlanta area.`, // AboutMe Detailed Description
-    testimonials: [
-        { quote: "Working with Linda was a game-changer for us. Her deep knowledge of the New York market and personalized approach made finding our dream home effortless. Highly recommended for anyone seeking a top-notch real estate experience.", author: "John and Emily R." },
-        { quote: "Linda Chrisp Realty provided exceptional service from start to finish. The team’s professionalism and attention to detail ensured that our property sale was smooth and successful. We couldn’t have asked for a better partner in real estate.", author: "Michael S." },
-        { quote: "The dedication and expertise of Linda were evident throughout our home-buying journey. Her ability to understand our needs and navigate the competitive market made the process enjoyable and stress-free. We are thrilled with our new home!", author: "Sarah and David L." },
-        { quote: "Linda and her team are truly outstanding. Their market knowledge, responsiveness, and client-focused approach set them apart. We have bought and sold multiple properties with them, and each experience has been exceptional.", author: "Jessica and Mark T." },
-        { quote: "If you’re looking for a real estate agent who goes above and beyond, Linda Chrisp is the one. Her dedication to finding the perfect property and ensuring a smooth transaction is unmatched. We are incredibly satisfied with the service we received.", author: "Laura K." }
-    ], // Testimonials
-    footerTagline: "Your Trusted Partner in Luxury Real Estate", // Footer Tagline
-    email: "", // Email
-    address: "1355 Terrell Mill Rd #1464, Marietta, GA 30067", // Address
-    phone: "770-899-6082", // Phone Number
-    footerDescription: "Linda Chrisp Realty is committed to delivering the highest standard of real estate services. Whether you're buying, selling, or investing in luxury properties, we offer unparalleled expertise and a personalized approach to meet your needs.", // Footer Description
+Customer Service excellence is our Passion!`,
+    testimonials: [{"quote": "Working with Laura Jelks was a game-changer for us. Her deep knowledge of the New York market and personalized approach made finding our dream home effortless. Highly recommended for anyone seeking a top-notch real estate experience.", "author": "John and Emily R."}, {"quote": "Laura Jelks provided exceptional service from start to finish. The teams professionalism and attention to detail ensured that our property sale was smooth and successful. We could not have asked for a better partner in real estate.", "author": "Michael S."}, {"quote": "The dedication and expertise of Laura Jelks were evident throughout our home-buying journey. Her ability to understand our needs and navigate the competitive market made the process enjoyable and stress-free. We are thrilled with our new home!", "author": "Sarah and David L."}, {"quote": "Laura Jelks and Her team are truly outstanding. Their market knowledge, responsiveness, and client-focused approach set them apart. We have bought and sold multiple properties with them, and each experience has been exceptional.", "author": "Jessica and Mark T."}, {"quote": "If you are looking for a real estate agent who goes above and beyond, Laura Jelks is the one. Her dedication to finding the perfect property and ensuring a smooth transaction is unmatched. We are incredibly satisfied with the service we received.", "author": "Laura K."}],
+    footerTagline: "Your Trusted Partner in Luxury Real Estate",
+    email: "",
+    address: "110 Inverness Corners \nBirminghamAL35242",
+    phone: "(205) 821-3384",
+    footerDescription: `Laura Jelks Realty is committed to delivering the highest standard of real estate services. Whether you're buying, selling, or investing in luxury properties, we offer unparalleled expertise and a personalized approach to meet your needs.`,
     socialMediaLinks: {
-        facebook: "",
+        facebook: "https://www.facebook.com/laura.c.jelks",
         Instagram: "",
         linkedin: "",
-    }, // Social Media Links
+    },
     contactDetails: {
         email: "",
-        phone: "770-899-6082",
-    }, // Contact Details
+        phone: "(205) 821-3384",
+    },
+    website: "",
+    dp: "https://scontent-sjc3-1.xx.fbcdn.net/v/t1.6435-9/72599358_10156322425730723_6147536104087093248_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=53a332&_nc_ohc=qIGlKz8XASMQ7kNvgFLPiwv&_nc_ht=scontent-sjc3-1.xx&oh=00_AYC1tBv_AM3OmieWq5seSVdydUQmuEmS2yxuxvJKNp-hGQ&oe=66F9942D" // Agent image URL
 };
 
 export default clientData;
