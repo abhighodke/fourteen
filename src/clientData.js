8823067fa752208c69bd8e509d558effbf34ba04
Abhishek Ghodke <abhighodke3@gmail.com>
2024-09-04 21:38:34 +0530

    client updated

--- src/clientData.js
+++ src/clientData.js
@@ -1,31 +1,31 @@
 // src/clientData.js
 const clientData = {
-    headerInitials: "LJ",
-    heroTagline: "Laura Jelks Realty",
-    agentName: "Laura Jelks",
+    headerInitials: "JS",
+    heroTagline: "Jessica Smith Realty",
+    agentName: "Jessica Smith",
     aboutMeShort: "I specialize in finding perfect homes for buyers and sellers, including investors and veterans. My expertise and dedication to exceptional service guide you to the best decisions. Let's find your ideal home together.",
     aboutMeDetailed: `It is my passion to assist clients with reaching their goal of owning a home or selling their current home to find a new one. It is important to me that my clients find the ideal home to match their needs and their budget. Also, I have helped many investors find the perfect property to complete their investment portfolio. 
 
 With my knowledge and experience I provide my clients accurate and complete detailed information so they can make the best decisions. Also, with being certified to help our Veterans and their families in finding a home and utilizing their full VA eligibility. Allow me to assist you with your real estate needs.
 
 Customer Service excellence is our Passion!`,
-    testimonials: [{"quote": "Working with Laura Jelks was a game-changer for us. Her deep knowledge of the New York market and personalized approach made finding our dream home effortless. Highly recommended for anyone seeking a top-notch real estate experience.", "author": "John and Emily R."}, {"quote": "Laura Jelks provided exceptional service from start to finish. The teams professionalism and attention to detail ensured that our property sale was smooth and successful. We could not have asked for a better partner in real estate.", "author": "Michael S."}, {"quote": "The dedication and expertise of Laura Jelks were evident throughout our home-buying journey. Her ability to understand our needs and navigate the competitive market made the process enjoyable and stress-free. We are thrilled with our new home!", "author": "Sarah and David L."}, {"quote": "Laura Jelks and Her team are truly outstanding. Their market knowledge, responsiveness, and client-focused approach set them apart. We have bought and sold multiple properties with them, and each experience has been exceptional.", "author": "Jessica and Mark T."}, {"quote": "If you are looking for a real estate agent who goes above and beyond, Laura Jelks is the one. Her dedication to finding the perfect property and ensuring a smooth transaction is unmatched. We are incredibly satisfied with the service we received.", "author": "Laura K."}],
+    testimonials: [{"quote": "Working with Jessica Smith was a game-changer for us. Her deep knowledge of the New York market and personalized approach made finding our dream home effortless. Highly recommended for anyone seeking a top-notch real estate experience.", "author": "John and Emily R."}, {"quote": "Jessica Smith provided exceptional service from start to finish. The teams professionalism and attention to detail ensured that our property sale was smooth and successful. We could not have asked for a better partner in real estate.", "author": "Michael S."}, {"quote": "The dedication and expertise of Jessica Smith were evident throughout our home-buying journey. Her ability to understand our needs and navigate the competitive market made the process enjoyable and stress-free. We are thrilled with our new home!", "author": "Sarah and David L."}, {"quote": "Jessica Smith and Her team are truly outstanding. Their market knowledge, responsiveness, and client-focused approach set them apart. We have bought and sold multiple properties with them, and each experience has been exceptional.", "author": "Jessica and Mark T."}, {"quote": "If you are looking for a real estate agent who goes above and beyond, Jessica Smith is the one. Her dedication to finding the perfect property and ensuring a smooth transaction is unmatched. We are incredibly satisfied with the service we received.", "author": "Laura K."}],
     footerTagline: "Your Trusted Partner in Luxury Real Estate",
-    email: "",
-    address: "110 Inverness Corners \nBirminghamAL35242",
-    phone: "(205) 821-3384",
-    footerDescription: `Laura Jelks Realty is committed to delivering the highest standard of real estate services. Whether you're buying, selling, or investing in luxury properties, we offer unparalleled expertise and a personalized approach to meet your needs.`,
+    email: "jessicasellsrealty@gmail.com",
+    address: "Mobile, AL, United States",
+    phone: "251-307-4198",
+    footerDescription: `Jessica Smith Realty is committed to delivering the highest standard of real estate services. Whether you're buying, selling, or investing in luxury properties, we offer unparalleled expertise and a personalized approach to meet your needs.`,
     socialMediaLinks: {
-        facebook: "https://www.facebook.com/laura.c.jelks",
+        facebook: "https://www.facebook.com/jessicasellsrealty/",
         Instagram: "",
         linkedin: "",
     },
     contactDetails: {
-        email: "",
-        phone: "(205) 821-3384",
+        email: "jessicasellsrealty@gmail.com",
+        phone: "251-307-4198",
     },
     website: "",
-    dp: "https://scontent-sjc3-1.xx.fbcdn.net/v/t1.6435-9/72599358_10156322425730723_6147536104087093248_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=53a332&_nc_ohc=qIGlKz8XASMQ7kNvgFLPiwv&_nc_ht=scontent-sjc3-1.xx&oh=00_AYC1tBv_AM3OmieWq5seSVdydUQmuEmS2yxuxvJKNp-hGQ&oe=66F9942D" // Agent image URL
+    dp: "https://scontent-sjc3-1.xx.fbcdn.net/v/t39.30808-6/239623604_1712888268904624_6204930320118260218_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=6CaM1O9C1lcQ7kNvgEDdSgO&_nc_ht=scontent-sjc3-1.xx&oh=00_AYB9qgEKry34RHQszZxOALruCDFlx7LfuadA4eiET-cuCg&oe=66DBD68D" // Agent image URL
 };
 
 export default clientData;
